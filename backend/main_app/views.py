2a9e9392a1aba45852eafd9bde9179edb5a48dc5
Mateus Padilha <mateuspadilha78@gmail.com>
2025-03-22 15:58:57 -0300

    estrutura inicial funcionando com view de teste

--- backend/main_app/views.py
+++ backend/main_app/views.py
@@ -1,8 +1,4 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Bem-vindo ao NutriTrack!")
-
-def sobre(request):
-    return HttpResponse("Página em construção")
+    return HttpResponse("NutriTrack está funcionando!")
